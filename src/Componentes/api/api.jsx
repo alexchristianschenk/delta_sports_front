baff0a750c61fea5679eb9d8c1290f367f6df9c7
alex <alex.schenk04@gmail.com>
2024-11-25 18:28:30 -0300

    users register

--- src/Componentes/api/api.jsx
+++ src/Componentes/api/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'https://delta-sports-back.onrender.com/api/users'
+    baseURL: 'https://delta-sports-back.onrender.com/api/'
 })
 
 export default api;
